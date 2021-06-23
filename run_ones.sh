d1b9eda51de1d40b173413362d863f83c36f66ba
Akane Murakawa <chenshjing@gmail.com>
2021-06-23 18:58:18 +0800

    Create run_ones.sh

---
+++ run_ones.sh
@@ -0,0 +1,31 @@
+#version 1.0
+#!/bin/bash
+
+# 更新依赖
+sudo apt-get update;
+sudo apt-get upgrade;
+
+# 安装工具
+sudo apt-get install net-tools ssh apt-transport-https ca-certificates curl gnupg lsb-release;
+
+# docker安装
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update;
+sudo apt-get install docker-ce docker-ce-cli containerd.io;
+
+# 添加hosts
+sudo su;
+echo "199.232.68.133 raw.githubusercontent.com" >> /etc/hosts
+
+
+# 执行jd-bash.sh
+# wget -q https://raw.githubusercontent.com/AkaneMurakawa/jd-base/main/jd-base.sh -O jd-bash.sh && chmod +x jd-bash.sh && ./jd-bash.sh
+# 搭建成功后访问： http://ip:5678  账号密码: admin/shuye72
+
+# 测试京东豆变化
+# docker exec jd bash jd jd_bean_change now
